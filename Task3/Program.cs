eeb5729bff54a4f9f55cd92f7933d40aa2e4b3e5
abotnari80 <abotnari80@gmail.com>
2022-04-10 13:15:09 +0300

    Задача

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿int[,,] mass = new int[2,2,2];
+int x = 10; int y = 19;
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        for (int n = 0; n < 2; n++)
+        {
+            mass[i,j,n] = new Random().Next(x,y);
+            x = x + 10; y = y + 10;
+            System.Console.Write($"{mass[i,j,n]}({i},{j},{n}) ");
+        }
+        System.Console.WriteLine();
+    }
+}
